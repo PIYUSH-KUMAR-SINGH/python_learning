95ec691986f173eda827817eef7bf39b6ba0a935
PIYUSH KUMAR SINGH <36163617+PIYUSH-KUMAR-SINGH@users.noreply.github.com>
2019-02-09 19:53:00 +0530

    Mathematical operations on two integers.

---
+++ python_basic_2.py
@@ -0,0 +1,12 @@
+# program to perform mathematical operations on two integers.
+print('Enter first number :')
+a = int(input())
+print('Enter second number :')
+b = int(input())
+
+add = a+b 
+diff = a-b
+prod = a*b
+div = a/b
+
+print('Addtion: ',add,'Subtraction: ',diff,'Product: ',prod,'Division: ',div,)
