2474777b51fffa3083132ac44f0d53c35750b96d
PIYUSH KUMAR SINGH <36163617+PIYUSH-KUMAR-SINGH@users.noreply.github.com>
2019-02-09 20:05:31 +0530

    Program to use if-else statements.

---
+++ python_basic_3.py
@@ -0,0 +1,7 @@
+#Program to use if-else statements.
+print('To check if integer is odd or even, enter an integer:')
+a = int(input())
+if a%2==0:
+    print('It is even')
+else:
+    print('It is odd')
